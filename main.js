2241d6e0ecff363078e674734313f8caf6f2280a
VladAlex95 <alex.56010@yahoo.com>
2022-04-02 14:47:11 +0300

    README.md

--- main.js
+++ main.js
@@ -1,40 +1,38 @@
 const arr = ['Rock','Paper','Scissors'];
 
-var ite = arr[Math.floor(Math.random()*arr.length)];
-let playerName =prompt("Name: ");
-let playerSelection;
-const computerSelection ='';
+var ite = arr[Math.floor(Math.random()*arr.length)]; //selectare aleatoare din vectorul arr
+let playerName ='';
+let playerSelection; 
+let  computerSelection ='';
 
-let W =0;
-let L=0;
+let W =0; //victorie
+let L=0;  //pierdere
 
 function computerPlay(){  
   //console.log(ite);
    return ite;
 }
+
+
 //const playerSelection ="Rock";
 
-//console.log(playRound(playerSelection,computerSleection));
+while (playerName === ''){
+  playerName =  prompt("Name: ");
+}
 
-
-//const computerSelection ="Rock";
-let egal =false;
-
-
+/*
 let capitalize =function(playerSelection){
   return  playerSelection && playerSelection[0].toUpperCase() + playerSelection.slice(1);
 }
-//alert(capitalize(playerSelection));
+*/
 
 
-function playRound(playerSelection,computerSelection){
+function playRound(playerSelection,computerSelection){//functie  pentru joc
     
-     playerSelection =prompt("");
-     playerSelection = capitalize(playerSelection);
+     
+     //playerSelection = capitalize(playerSelection);
        
-     computerSelection = computerPlay();
-
-   
+     //computerSelection = computerPlay();
 
    if (playerSelection ==="Paper" && computerSelection ==="Rock"){
       
@@ -42,29 +40,29 @@ function playRound(playerSelection,computerSelection){
     W++;
     
    }
-      else   if (playerSelection ==="Scissors" && computerSelection ==="Paper"){
+      else  if (playerSelection ==="Scissors" && computerSelection ==="Paper"){
        console.log("You Win! Scissors beats Paper");
          W++
        }
 
-       else   if  (playerSelection ==="Rock" && computerSelection ==="Scissors"){
+       else  if  (playerSelection ==="Rock" && computerSelection ==="Scissors"){
        console.log("You Win!  Rock beats Scissors");
          W++;
        }
 
 
-        else   if (playerSelection ==="Rock" && computerSelection ==="Paper"){
+        else  if (playerSelection ==="Rock" && computerSelection ==="Paper"){
         
         console.log("You Lose! Paper beats Rock");
         L++;
         
         }
-        else   if (playerSelection ==="Paper" && computerSelection ==="Scissors"){
+        else  if (playerSelection ==="Paper" && computerSelection ==="Scissors"){
            console.log("You Lose! Scissors beats Paper");
            L++;
           }
     
-        else   if  (playerSelection ==="Scissors" && computerSelection ==="Rock"){
+        else  if  (playerSelection ==="Scissors" && computerSelection ==="Rock"){
            console.log("You Lose!  Rock beats Scissors");
            L++;
           
@@ -81,29 +79,82 @@ function playRound(playerSelection,computerSelection){
  
    }
 
-function game(){
-    for (let i =0;i<5;i++){
 
-        playRound(playerSelection,computerSelection); 
-    
-  }
-   
-}
+  function ButonClick(){ //verifica butonul apasat
+    document.getElementById('Rock').addEventListener("click",function(){
+    playerSelection = "Rock"; 
+    computerSelection=computerPlay();
+    playRound( playerSelection,computerSelection);
+    document.getElementById('player').innerText=W;
+    document.getElementById('computer').innerText=L;
+      if (W ===5 || L ==5){ 
+
+        if (W > L){  //verificare castigator joc
+          //console.log(`the winner is ${playerName}`);
+        
+          alert(`The Winner is ${playerName}!`);
+        }
+        else{
+          //console.log(`the winner is Computer`);
+          alert(`The Winner is Computer!`);
+        }
 
 
+      }
+      console.log(W);
+      console.log(L);
+      //console.log('rock button  was clicked!');
+     });
+     document.getElementById('Paper').addEventListener("click",function(){
+      playerSelection = "Paper"; 
+      computerSelection=computerPlay();
+      playRound( playerSelection,computerSelection);
+      document.getElementById('player').innerText=W;
+      document.getElementById('computer').innerText=L;
+      if (W ===5 || L ==5){
 
-game();
-if (W > L){
-    //console.log(`the winner is ${playerName}`);
+        if (W > L){
+          //console.log(`the winner is ${playerName}`);
+        
+          alert(`The Winner is ${playerName}!`);
+        }
+        else{
+          //console.log(`the winner is Computer`);
+          alert(`The Winner is Computer!`);
+        }
 
-    alert(`the winner is ${playerName}`);
-}
-else{
-    //console.log(`the winner is Computer`);
-    alert(`the winner is Computer`);
-}
-//console.log(computerPlay());
+
+      }
+      console.log(W);
+      console.log(L)
+      //console.log('paper button  was clicked!');
+     });
+     
+     document.getElementById('Scissors').addEventListener("click",function(){
+      playerSelection = "Scissors"; 
+      computerSelection=computerPlay();
+      playRound( playerSelection,computerSelection);
+      document.getElementById('player').innerText=W;
+      document.getElementById('computer').innerText=L;
+      if (W ===5 || L ==5){
+
+        if (W > L){
+          //console.log(`the winner is ${playerName}`);
+        
+          alert(`The Winner is ${playerName}!`);
+        }
+        else{
+          //console.log(`the winner is Computer`);
+          alert(`The Winner is Computer!`);
+        }
+
+      }
+      console.log(W);
+      console.log(L)
+      //console.log('scissors button  was clicked!');
+     });
+
+    }
+ 
 //playRound(playerSelection,computerSelection);
-
-
-
+ButonClick();
